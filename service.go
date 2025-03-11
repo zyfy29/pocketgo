d4eac9696099f102284a25fba0a962dcad8b0b49
zyfy29 <wasuremono127@gmail.com>
2025-03-11 19:01:05 +0900

    refactor: rename formatter to responseFormatter
    Updated all instances of 'formatter' to 'responseFormatter' for clarity and consistency. The changes also involved adjusting the method signatures and comments for better understanding of the response formatting process.

--- service.go
+++ service.go
@@ -1,7 +1,7 @@
 package pocket
 
 func (c *Client) ValidateToken() (string, error) {
-	content, err := formatter[UserInfoItem]{c}.doRestWithResult("", "https://pocketapi.48.cn/user/api/v1/user/info/reload", map[string]string{})
+	content, err := responseFormatter[UserInfoItem]{c}.doRestWithResult("", "https://pocketapi.48.cn/user/api/v1/user/info/reload", map[string]string{})
 	if err != nil {
 		return "", err
 	}
@@ -9,7 +9,7 @@ func (c *Client) ValidateToken() (string, error) {
 }
 
 func (c *Client) GetHistoryLives(memberID string, nextTime int64) ([]LiveItem, int64, error) {
-	content, err := formatter[LiveListContent]{c}.doRestWithResult("", "https://pocketapi.48.cn/im/api/v1/chatroom/msg/list/aim/type", newLiveListReq(memberID, nextTime))
+	content, err := responseFormatter[LiveListContent]{c}.doRestWithResult("", "https://pocketapi.48.cn/im/api/v1/chatroom/msg/list/aim/type", newLiveListReq(memberID, nextTime))
 	if err != nil {
 		return nil, 0, err
 	}
@@ -21,7 +21,7 @@ func (c *Client) GetHistoryLives(memberID string, nextTime int64) ([]LiveItem, i
 }
 
 func (c *Client) GetLiveInfo(liveId string) (LiveInfoContent, error) {
-	content, err := formatter[LiveInfoContent]{c}.doRestWithResult("", "https://pocketapi.48.cn/live/api/v1/live/getLiveOne", map[string]string{
+	content, err := responseFormatter[LiveInfoContent]{c}.doRestWithResult("", "https://pocketapi.48.cn/live/api/v1/live/getLiveOne", map[string]string{
 		"liveId": liveId,
 	})
 	if err != nil {
@@ -31,7 +31,7 @@ func (c *Client) GetLiveInfo(liveId string) (LiveInfoContent, error) {
 }
 
 func (c *Client) GetVoiceStatus(channelId, serverId int) (VoiceStatusContent, error) {
-	content, err := formatter[VoiceStatusContent]{c}.doRestWithResult("", "https://pocketapi.48.cn/im/api/v1/team/voice/operate", map[string]int{
+	content, err := responseFormatter[VoiceStatusContent]{c}.doRestWithResult("", "https://pocketapi.48.cn/im/api/v1/team/voice/operate", map[string]int{
 		"channelId":   channelId,
 		"serverId":    serverId,
 		"operateCode": 2,
@@ -43,7 +43,7 @@ func (c *Client) GetVoiceStatus(channelId, serverId int) (VoiceStatusContent, er
 }
 
 func (c *Client) GetMessageList(channelId, serverId int, nextTime int64) ([]MessageItem, int64, error) {
-	content, err := formatter[MessageContent]{c}.doRestWithResult("", "https://pocketapi.48.cn/im/api/v1/team/message/list/homeowner", map[string]any{
+	content, err := responseFormatter[MessageContent]{c}.doRestWithResult("", "https://pocketapi.48.cn/im/api/v1/team/message/list/homeowner", map[string]any{
 		"channelId": channelId,
 		"serverId":  serverId,
 		"nextTime":  nextTime,
@@ -54,7 +54,7 @@ func (c *Client) GetMessageList(channelId, serverId int, nextTime int64) ([]Mess
 	}
 	res := []MessageItem{}
 	for _, msg := range content.Message {
-		msg.FillExtInfo()
+		msg.fillExtInfo()
 		res = append(res, msg)
 	}
 	return res, content.NextTime, nil
